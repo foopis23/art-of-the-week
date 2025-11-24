2c663cb06637106e594f852db8558f3db3f918d5
Eric Sexton <foopis23@gmail.com>
2025-11-24 15:02:35 -0500

    added remote logging

--- src/log.ts
+++ src/log.ts
@@ -1,9 +1,22 @@
 import { env } from '@/env'
 import pino from 'pino'
 
+const devTransport = {
+  target: 'pino-pretty',
+}
+
+const prodTransport = env.SENTRY_DSN
+  ? {
+      target: 'pino-sentry-transport',
+      options: {
+        sentry: {
+          dsn: env.SENTRY_DSN,
+        },
+      },
+    }
+  : undefined
+
 export const log = pino({
   level: env.NODE_ENV === 'development' ? 'debug' : 'info',
-  transport: {
-    target: 'pino-pretty',
-  },
+  transport: env.NODE_ENV === 'development' ? devTransport : prodTransport,
 })
